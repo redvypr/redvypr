d10fcd6d7c1f36b16993cf8ccdeb28067504f898
pholterm <peter.holtermann@systemausfall.org>
2021-12-30 17:50:17 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,7 @@ extensions = ['sphinx.ext.napoleon','sphinx.ext.autosectionlabel','sphinx.ext.au
 
 autosummary_generate = True
 
-autodoc_mock_imports = ["time", "serial","serial.tools","datetime","logging","queue","yaml","PyQt5","pkg_resources","pyqtgraph","numpy","inspect","threading","multiprocessing","socket","argparse","importlib","glob","pathlib","signal","QtWidgets", "QtCore", "QtGui","redvyprdevices","redvypr.devices","netCDF4","copy"]
+autodoc_mock_imports = ["time", "serial","serial.tools","datetime","logging","queue","yaml","PyQt5","pkg_resources","pyqtgraph","numpy","inspect","threading","multiprocessing","socket","argparse","importlib","glob","pathlib","signal","QtWidgets", "QtCore", "QtGui","netCDF4","copy","threading","socket"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
