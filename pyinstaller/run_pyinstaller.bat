0ac3d4ea0d0c59b4a31cfe5f94c1b1dec1989f73
Peter Holtermann <peter.holtermann@io-warnemuende.de>
2023-04-25 20:07:07 +0200

    docs

---
+++ pyinstaller/run_pyinstaller.bat
@@ -0,0 +1,7 @@
+:: One file version
+::pyinstaller --onefile -i ../redvypr/icon/icon_v03.2.ico --workpath ./build_windows --distpath ./dist_windows --noconfirm --add-data "../redvypr/VERSION;." --add-data "../redvypr/icon/*;." --hidden-import cftime --hidden-import netCDF4.utils  --clean -n redvypr redvypr_run.py
+
+:: Standard version
+pyinstaller -i ../redvypr/icon/icon_v03.2.ico --workpath ./build_windows --distpath ./dist_windows --noconfirm --add-data "../redvypr/VERSION;." --add-data "../redvypr/icon/*;." --hidden-import cftime --hidden-import netCDF4.utils  --clean -n redvypr redvypr_run.py
+
+:: Windows 10 mkl_intel_thread.1.dll in anaconda/Library/bin has to be copied into the redvypr folder in dist
--- redvypr/pyinstaller/run_pyinstaller.bat
+++
@@ -1,4 +0,0 @@
-:: One file version
-pyinstaller --onefile -i ../icon/icon_v03.2.ico --workpath ./build_windows --distpath ./dist_windows --noconfirm --add-data "../VERSION;." --add-data "../icon/*;." --hidden-import cftime --hidden-import netCDF4.utils  --clean -n redvypr redvypr_run.py
-
-:: Windows 10 mkl_intel_thread.1.dll in anaconda/Library/bin has to be copied into the redvypr folder in dist
