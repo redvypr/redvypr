8a298aed9173e94ed43e3c75320b1d048b2858a4
peter <peter.holtermann@posteo.net>
2024-10-06 20:00:20 +0200

    removed utils/csv2dict

--- redvypr/__init__.py
+++ redvypr/__init__.py
@@ -6,7 +6,6 @@ from . import gui
 from .widgets import *
 from . import files
 #from . import standard_device_widgets
-from . import utils
 from . import redvypr_main
 #from .data_packets import redvypr_datadict
 from . import version
--- redvypr/utils/csv2dict/__init__.py
+++
@@ -1,1 +0,0 @@
-from .csv2dict import *
