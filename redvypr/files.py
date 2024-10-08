7b2e838dcb501147502671b011f655872425151e
peter <peter.holtermann@posteo.net>
2024-10-08 09:33:48 +0200

    pyinstaller icon files

--- redvypr/files.py
+++ redvypr/files.py
@@ -22,24 +22,24 @@ def resource_path(relative_path):
 # Until v 0.3.11
 #logo_file = pkg_resources.resource_filename('redvypr','icon/redvypr_logo_v02.png')
 # Desert Horned Viper
-logo_file = pkg_resources.resource_filename('redvypr','icon/logo_v03.1.png')
+logo_file = pkg_resources.resource_filename('redvypr','icon/logo_v03.1.svg')
 # This is a workaround to read the VERSION file in a pyinstaller environment in linux (redvypr exectuable and redvypr directory cannot life together)
 if(os.path.exists(logo_file)):
     pass
 else:
-    logo_file = resource_path('redvypr_logo_v02.png')
+    logo_file = resource_path('redvypr_logo_v02.svg')
     if (os.path.exists(logo_file)):
         pass
     else:  # pyinstaller windows10
         logger.warning('Could not load logo file {}'.format(logo_file))
     
 # Desert Horned Viper
-icon_file = pkg_resources.resource_filename('redvypr','icon/icon_v03.1.png')
+icon_file = pkg_resources.resource_filename('redvypr','icon/icon_v03.1.svg')
 # This is a workaround to read the VERSION file in a pyinstaller environment in linux (redvypr exectuable and redvypr directory cannot life together)
 if(os.path.exists(icon_file)):
     pass
 else:
-    icon_file = resource_path('icon_v03.1.png')
+    icon_file = resource_path('icon_v03.1.svg')
     if (os.path.exists(icon_file)):
         pass
     else:  # pyinstaller windows10
